e6473c329fe1475fbb74cd335aabfa09fa405d2a
Ira Ko <irako.jf@gmail.com>
2026-03-03 22:26:30 -0500

    fix(writers): export PDF table models for reconciliation reports (bd-20b.3)

--- src/cpapacket/writers/__init__.py
+++ src/cpapacket/writers/__init__.py
@@ -2,13 +2,15 @@
 
 from .csv_writer import CsvWriter, CsvWriterConfig
 from .json_writer import JsonWriter, JsonWriterConfig
-from .pdf_writer import PdfWriter, PdfWriterConfig
+from .pdf_writer import PdfTableRow, PdfTableSection, PdfWriter, PdfWriterConfig
 
 __all__ = [
     "CsvWriter",
     "CsvWriterConfig",
     "JsonWriter",
     "JsonWriterConfig",
+    "PdfTableRow",
+    "PdfTableSection",
     "PdfWriter",
     "PdfWriterConfig",
 ]
